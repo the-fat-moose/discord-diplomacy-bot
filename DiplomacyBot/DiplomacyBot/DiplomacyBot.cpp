108a1af7e9345db76971b8e1976dc0d14c3591b7
Moose <yugihanmaster321@gmail.com>
2024-02-03 22:16:47 -0500

    slash command setup

--- DiplomacyBot/DiplomacyBot/DiplomacyBot.cpp
+++ DiplomacyBot/DiplomacyBot/DiplomacyBot.cpp
@@ -1,5 +1,4 @@
 // DiplomacyBot.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <fstream>
@@ -24,25 +23,59 @@ int main()
     /* Output simple log messages to stdout */
     bot.on_log(dpp::utility::cout_logger());
 
-    /* Handle slash command */
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+    /* Handle simple slash command example */
+    bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {  
         if (event.command.get_command_name() == "ping")
         {
             event.reply("Pong!");
         }
     });
 
-    /* Register slash command here in on_ready */
+    /* Handle embed command for the github repository */
+    bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
+        /* Check if the gitrepo command was called */
+        if (event.command.get_command_name() == "gitrepo")
+        {
+            /* Create an embed */
+            dpp::embed embed = dpp::embed()
+                .set_color(dpp::colors::purple)
+                .set_title("Git Hub Repository")
+                .set_url("https://github.com/the-fat-moose/discord-diplomacy-bot/")
+                .set_author("Moose", "https://github.com/the-fat-moose/", "https://i.imgur.com/RFauyY6.png/")
+                .set_description("Here is my repository!")
+                .set_thumbnail("https://i.imgur.com/dJc2Rju.png/")
+                .set_timestamp(time(0));
+
+            /* Create a message with the content as our new embed */
+            dpp::message msg(event.command.channel_id, embed);
+
+            /* Reply to the user with the message, containing our embed */
+            event.reply(msg);
+        }
+    });
+
+    /* Handle command to display an image of the map */
+    bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
+        /* Check if the displaymap command was called */
+        if (event.command.get_command_name() == "displaymap")
+        {
+            // do something smile :D
+        }
+    });
+
+    /* Register slash commands here in on_ready */
     bot.on_ready([&bot](const dpp::ready_t& event) {
         /* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
         if (dpp::run_once<struct register_bot_commands>())
         {
             bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+            bot.global_command_create(dpp::slashcommand("gitrepo", "Check out my repository!", bot.me.id));
+            bot.global_command_create(dpp::slashcommand("displaymap", "Displays an up to date game map", bot.me.id));
         }
     });
 
     /* Start the bot */
-    bot.start(false);
+    bot.start(dpp::st_wait);
 
     return 0;
 }
